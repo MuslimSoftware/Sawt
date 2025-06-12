ca39d58ed58d184ca907c313640c9b40bdd3550b
Younes Benketira (PERSONAL) <admin@younesbenketira.com>
2025-06-11 20:40:59 -0400

    Refactor input

--- Core/input/automation/target/debug/deps/cfg_if-09d4f236cce38ff9.d
+++
@@ -1,7 +0,0 @@
-/Users/younesbenketira/Code/personal/Sawt/Core/input/automation/target/debug/deps/libcfg_if-09d4f236cce38ff9.rmeta: /Users/younesbenketira/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.1/src/lib.rs
-
-/Users/younesbenketira/Code/personal/Sawt/Core/input/automation/target/debug/deps/libcfg_if-09d4f236cce38ff9.rlib: /Users/younesbenketira/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.1/src/lib.rs
-
-/Users/younesbenketira/Code/personal/Sawt/Core/input/automation/target/debug/deps/cfg_if-09d4f236cce38ff9.d: /Users/younesbenketira/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.1/src/lib.rs
-
-/Users/younesbenketira/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.1/src/lib.rs:
--- Core/input/automation/target/debug/deps/hound-d0eba7f493906121.d
+++
@@ -1,9 +0,0 @@
-/Users/younesbenketira/Code/personal/Sawt/Core/input/automation/target/debug/deps/libhound-d0eba7f493906121.rmeta: /Users/younesbenketira/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hound-3.5.1/src/lib.rs /Users/younesbenketira/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hound-3.5.1/src/read.rs /Users/younesbenketira/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hound-3.5.1/src/write.rs
-
-/Users/younesbenketira/Code/personal/Sawt/Core/input/automation/target/debug/deps/libhound-d0eba7f493906121.rlib: /Users/younesbenketira/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hound-3.5.1/src/lib.rs /Users/younesbenketira/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hound-3.5.1/src/read.rs /Users/younesbenketira/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hound-3.5.1/src/write.rs
-
-/Users/younesbenketira/Code/personal/Sawt/Core/input/automation/target/debug/deps/hound-d0eba7f493906121.d: /Users/younesbenketira/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hound-3.5.1/src/lib.rs /Users/younesbenketira/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hound-3.5.1/src/read.rs /Users/younesbenketira/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hound-3.5.1/src/write.rs
-
-/Users/younesbenketira/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hound-3.5.1/src/lib.rs:
-/Users/younesbenketira/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hound-3.5.1/src/read.rs:
-/Users/younesbenketira/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hound-3.5.1/src/write.rs:
